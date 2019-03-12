50e34de9f2d51dcd4dcfe9d200f1ebac40960369
Jack Biscupski <jack-biscupski@uiowa.edu>
2019-03-11 20:42:53 -0500

    changed structure of flask files

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,6 @@
-# beginner/mess around stuff with flask - may update to actual
-# main code file at some point
+from app import app
+from flask import render_template
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -18,6 +15,3 @@ def route():
 def harden():
     player = {"name": "James Harden"}
     return render_template("harden.html", user=player)
-
-if __name__ == "__main__":
-    app.run(debug=True)
