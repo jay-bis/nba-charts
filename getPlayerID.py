2f284fd7b503a51f3cf182c23b55ddb89b02d4c5
Jack Biscupski <jack-biscupski@uiowa.edu>
2019-11-09 14:57:26 -0600

    Update getPlayerID.py

--- getPlayerID.py
+++ getPlayerID.py
@@ -5,11 +5,18 @@ Created on Wed Jan  9 13:37:32 2019
 @author: Jack Biscupski
 """
 
-from nba_api.stats.static import players
+from nba_api.stats.static import players, teams
 
+# simple utility function to wrap nba_api player finder into easy, nice function
 def playerID(fullName):
     # find player by full name
     playerDict = players.find_players_by_full_name(fullName)
     
     return playerDict[0]['id']
-    
+
+def teamID(teamName):
+    # find team ID by team name
+   teamDict = teams.find_teams_by_full_name(teamName)
+   if len(teamDict) == 0:
+       teamDict = teams.find_teams_by_nickname(teamName)
+   return  teamDict[0]['id']
